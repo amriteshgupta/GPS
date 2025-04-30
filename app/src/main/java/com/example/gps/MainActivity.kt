c2ba1380a37870805d6c4c81969b6f1db40d796a
Amritesh <blpamritesh@gmail.com>
2025-04-30 21:49:18 +0530

    Some major UI Changes

--- app/src/main/java/com/example/gps/MainActivity.kt
+++ app/src/main/java/com/example/gps/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gps.databinding.ActivityMainBinding
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var geoCoder: Geocoder
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen= installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
